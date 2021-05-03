5b7e23cd8c696be1e61587b8b621a8e5ab282f56
Txs1111 <1831069264@qq.com>
2021-05-03 10:01:58 +0800

    创建工具类实现转换时间

---
+++ ssm_integrate/src/main/java/com/T_Lyon/util/switchTime.java
@@ -0,0 +1,13 @@
+package com.T_Lyon.util;
+
+import com.T_Lyon.pojo.User;
+
+public class switchTime {
+    public User Time_to_String(User user) {
+        return user;
+    }
+
+    public User String_to_Time(User user) {
+        return user;
+    }
+}
