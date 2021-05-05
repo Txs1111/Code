87a915bcf370d673cbaac87fecb63500dd1ffbed
Txs1111 <1831069264@qq.com>
2021-05-05 19:48:46 +0800

    注册控制器

---
+++ ssm_integrate/src/main/java/com/T_Lyon/controller/registerController.java
@@ -0,0 +1,20 @@
+package com.T_Lyon.controller;
+
+import com.T_Lyon.pojo.User;
+import com.T_Lyon.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+public class registerController {
+
+    @Autowired
+    @Qualifier("UserServiceImpl")
+    private UserService userService;
+
+    @RequestMapping("/")
+}
