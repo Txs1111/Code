0b81530e1372b2e03df865d0457a0d630c506de2
Txs1111 <1831069264@qq.com>
2021-05-08 21:02:55 +0800

    比较=与equals的区别

---
+++ TestJava/Tool/tool/com/T_Lyon/equals/equals.java
@@ -0,0 +1,34 @@
+package com.T_Lyon.equals;
+
+public class equals {
+    public equals() {
+    }
+
+
+    public void equals() {
+
+    }
+
+    public static void main(String[] args) {
+
+        //    比较两个对象是否相等
+        Integer integer = new Integer(1);
+        Integer integer2 = new Integer(1);
+        System.out.println(integer == integer2);
+        System.out.println(integer.byteValue() == integer2.byteValue());
+        System.out.println(integer.equals(integer2));
+
+        int a = 1;
+        int b = 1;
+
+
+        System.out.println(a == b);
+
+        String A = " ";
+        String B = " ";
+        System.out.println(A == B);
+        System.out.println(A.toString() == B.toString());
+        System.out.println(A.equals(B));
+    }
+
+}
