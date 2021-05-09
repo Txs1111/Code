e8b17e3e9860d274ed760ff120a1ff28e613aa46
Txs1111 <1831069264@qq.com>
2021-05-09 15:06:38 +0800

    测试两个public类

--- TestJava/Tool/tool/com/T_Lyon/doublePublicClass/doublePublicClass.java
+++ TestJava/Tool/tool/com/T_Lyon/doublePublicClass/doublePublicClass.java
@@ -2,6 +2,8 @@ package com.T_Lyon.doublePublicClass;
 
 public class doublePublicClass {
     public int aa() {
+        float[] a = new float[3];
+        a = new float[]{1f, 1f, 1f};
         return 1;
     }
 }
